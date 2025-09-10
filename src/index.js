5334da1548723e26d3dd3c8ee02086dde7c96498
khushi-kukreja <khushikuk123@gmail.com>
2025-09-10 21:18:05 +0530

    Fix routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import AppContextProvider from './context/AppContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/HikVision">
     <AppContextProvider>
     <App />
     </AppContextProvider>
